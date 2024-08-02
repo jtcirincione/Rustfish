3714f666135232754c49e4e1dfd9d76c143118ca
John Cirincione <jtcirinc@ncsu.edu>
2024-08-01 22:34:03 -0400

    Added moving functionality to frontend

--- client/src/Chessboard.js
+++ client/src/Chessboard.js
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import bR from './images/bR.png';
 import bN from './images/bN.png';
 import bB from './images/bB.png';
@@ -15,28 +15,31 @@ import wp from './images/wp.png';
 function Chessboard() {
 
     // Number of rows
-    const n = 8; 
+    const n = 8;
     // Number of columns
     const m = 8;
- 
+
     // Array which will be used to 
     // display the chessboard
     const [chessBoard, setChessBoard] = useState([]);
-    const [images, initializeImages] = useState([])
+    const [images, initializeImages] = useState([]);
+    const [clickedPiece, setClickedPiece] = useState(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-    
+
 
     useEffect(() => {
- 
+
         // Initialize result with an empty array
         const result = [];
- 
+
         for (let i = 0; i < n; i++) {
 
             const row = Array.from({ length: m });
             result.push(row);
         }
- 
+
 
         setChessBoard(result);
         let images = [
@@ -49,11 +52,56 @@ function Chessboard() {
             wp, wp, wp, wp, wp, wp, wp, wp,
             wR, wN, wB, wQ, wK, wB, wN, wR
         ];
+
+
+        const handleMouseMove = (e) => {
+            setMousePosition({ x: e.clientX, y: e.clientY });
+        };
+
         initializeImages(images);
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
     }, []);
 
-    
+
     function bitBoardToImages(bitBoard) {
+
+    }
+
+    function onDown(e, piece, idx) {
+        e.preventDefault();
+        if (!piece) {
+            return
+        }
+        setIsDragging(true);
+        setClickedPiece({piece, idx});
+        images[idx] = null;
+    }
+
+    function movePiece(e, idx) {
+        if (!clickedPiece || !isDragging) {
+            setClickedPiece(null)
+            setIsDragging(false)
+            return
+        }
+        if (idx === clickedPiece.idx) {
+            if (images[idx] == null) {
+                images[idx] = clickedPiece.piece;
+            }
+            setClickedPiece(null)
+            setIsDragging(false)
+            return
+        }
+        images[idx] = clickedPiece.piece;
+        images[clickedPiece.idx] = null;
+        setIsDragging(false);
+        setClickedPiece(null);
+        initializeImages(images);
+
 
     }
 
@@ -66,17 +114,16 @@ function Chessboard() {
                             const piece = images[rIndex * 8 + cIndex];
                             return (
                                 <div
-                                    className={`w-[80px] h-[80px] flex items-center justify-center ${
-                                        (rIndex + cIndex) % 2 === 0
-                                            ? "bg-chessGreen" : "bg-white"
-                                    }`}
-                                    key={cIndex}
+                                    className={`w-[80px] h-[80px] flex items-center justify-center ${(rIndex + cIndex) % 2 === 0
+                                        ? "bg-chessGreen" : "bg-white"
+                                        }`}
+                                    key={cIndex} onMouseUp={(e) => movePiece(e, rIndex * 8 + cIndex)} onMouseDown={(e) => onDown(e, piece, rIndex * 8 + cIndex)}
                                 >
                                     {piece ? (
-                                        <img 
-                                            src={piece} 
-                                            alt={`piece-${rIndex}-${cIndex}`} 
-                                            className="object-contain" 
+                                        <img
+                                            src={piece}
+                                            alt={`piece-${rIndex}-${cIndex}`}
+                                            className="object-contain"
                                         />
                                     ) : null}
                                 </div>
@@ -84,6 +131,22 @@ function Chessboard() {
                         })}
                     </div>
                 ))}
+
+
+            {isDragging && clickedPiece && (
+                <img
+                    src={clickedPiece.piece}
+                    alt={clickedPiece.piece}
+                    className="object-contain absolute"
+                    style={{
+                        left: mousePosition.x - 40,
+                        top: mousePosition.y - 40,
+                        width: "80px",
+                        height: "80px",
+                        pointerEvents: "none",
+                    }}
+                />
+            )}
         </>
     );
 
