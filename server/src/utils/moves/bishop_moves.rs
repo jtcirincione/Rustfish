510a2025ad4a0925bfced23c24090c47cc63a1d7
John Cirincione <jtcirinc@ncsu.edu>
2025-03-27 22:43:06 -0400

    Cleaning house

--- server/src/utils/moves/bishop_moves.rs
+++
@@ -1,5 +0,0 @@
-use crate::utils::moves::sliding_piece_moves;
-pub fn generate_pseudo_moves(my_bishops: u64, enemy: u64, occupied: u64, white: bool) -> u64 {
-    let my_pieces = !enemy & occupied;
-    return sliding_piece_moves::d_anti_moves(my_bishops, occupied) & !my_pieces;
-}
--- server/src/utils/moves/rook_moves.rs
+++
@@ -1,5 +0,0 @@
-use crate::utils::moves::*;
-pub fn generate_pseudo_moves(my_rooks: u64, enemy: u64, occupied: u64, white: bool) -> u64 {
-    let my_pieces = !enemy & occupied;
-    return sliding_piece_moves::h_v_moves(my_rooks, occupied) & !my_pieces;
-}
